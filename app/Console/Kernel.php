adc9396ba7f8ed5095d1ae79f13a53a9f2b2a069
mohamamad <mohamad.shahrokhi@gmail.com>
2018-05-27 18:33:02 +0430

    Minor

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,143 +36,143 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-//        $smsNumber = config("constants.SMS_PROVIDER_DEFAULT_NUMBER");
-//        $schedule->call(function () use( $smsNumber ){
-//            $dayOfWeekJalali = $this->convertToJalaliDay(Carbon::today('Asia/Tehran')->format('l')) ;
-//            $toDayDate = Carbon::today('Asia/Tehran')->format("Y-m-d") ;
-//            $employees = User::whereHas("roles" , function ($q){
-//                $q->where("name" , Config::get("constants.EMPLOYEE_ROLE")) ;
-//            })->get();
-//            foreach ($employees as $employee)
-//            {
-//                $employeeSchedule = Employeeschedule::where("user_id", $employee->id)->where("day" , $dayOfWeekJalali)->get()->first();
-//                $employeeTimeSheet = Employeetimesheet::where("user_id" , $employee->id)->where("date" , $toDayDate)->get();
-//                $done = false ;
-//                if($employeeTimeSheet->isEmpty()){
-//                    $newEmplployeeTimeSheet = new Employeetimesheet();
-//
-//                    $newEmplployeeTimeSheet->date = $toDayDate;
-//                    $newEmplployeeTimeSheet->user_id = $employee->id ;
-//                    if(isset($employeeSchedule))
-//                    {
-//                        $newEmplployeeTimeSheet->userBeginTime = $employeeSchedule->getOriginal("beginTime");
-//                        $newEmplployeeTimeSheet->userFinishTime = $employeeSchedule->getOriginal("finishTime");
-//                        $newEmplployeeTimeSheet->allowedLunchBreakInSec = $employeeSchedule->getOriginal("lunchBreakInSeconds");
-//                    }
-//                    $newEmplployeeTimeSheet->clockIn = "00:00:00";
-//                    $newEmplployeeTimeSheet->beginLunchBreak = "00:00:00";
-//                    $newEmplployeeTimeSheet->finishLunchBreak = "00:00:00";
-//                    $newEmplployeeTimeSheet->clockOut = "00:00:00";
-//                    $newEmplployeeTimeSheet->breakDurationInSeconds = 0 ;
-//                    $newEmplployeeTimeSheet->managerComment = "ثبت توسط سیستم : مرخصی یا تعطیلی غیر رسمی";
-//                    $newEmplployeeTimeSheet->employeeComment = null;
-//                    $newEmplployeeTimeSheet->timeSheetLock = 1;
-//                    $newEmplployeeTimeSheet->isPaid = 1;
-//                    $newEmplployeeTimeSheet->workdaytype_id = 1;
-//
-//                    if($newEmplployeeTimeSheet->save())
-//                        $done = $newEmplployeeTimeSheet->id;
-//                    else
-//                        $done = false;
-//                }elseif(!$employeeTimeSheet->first()->getOriginal("timeSheetLock")){
-//                    $employeeTimeSheet = $employeeTimeSheet->first();
-//                    if(strcmp($employeeTimeSheet->clockIn , "00:00:00") == 0 )
-//                    {
-//                        if(strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") != 0 )
-//                            $employeeTimeSheet->clockIn = $employeeTimeSheet->beginLunchBreak ;
-//                        elseif(strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") != 0 )
-//                            $employeeTimeSheet->clockIn = $employeeTimeSheet->finishLunchBreak ;
-//                        elseif(strcmp($employeeTimeSheet->clockOut , "00:00:00") != 0 )
-//                            $employeeTimeSheet->clockIn = $employeeTimeSheet->clockOut ;
-//                    }
-//                    if(strcmp($employeeTimeSheet->clockOut , "00:00:00") == 0 )
-//                    {
-//                        if(strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") != 0 )
-//                            $employeeTimeSheet->clockOut = $employeeTimeSheet->finishLunchBreak ;
-//                        elseif(strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") != 0 )
-//                            $employeeTimeSheet->clockOut = $employeeTimeSheet->beginLunchBreak ;
-//                        elseif(strcmp($employeeTimeSheet->clockIn , "00:00:00") != 0 )
-//                            $employeeTimeSheet->clockOut = $employeeTimeSheet->clockIn ;
-//                    }
-//
-//                    if(strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") == 0 &&
-//                        strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") != 0)
-//                    {
-//                        if(strcmp($employeeTimeSheet->clockIn , "00:00:00") != 0 )
-//                            $employeeTimeSheet->beginLunchBreak = $employeeTimeSheet->clockIn ;
-//                    }
-//
-//                    if(strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") == 0 &&
-//                        strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") != 0)
-//                    {
-//                        if(strcmp($employeeTimeSheet->clockOut , "00:00:00") != 0 )
-//                            $employeeTimeSheet->finishLunchBreak = $employeeTimeSheet->clockOut ;
-//                    }
-//
-//                    $employeeTimeSheet->managerComment = $employeeTimeSheet->managerComment." ثبت توسط سیستم : مرخصی یا تعطیلی غیر رسمی";
-//                    $employeeTimeSheet->timeSheetLock = 1;
-//
-//                    if($employeeTimeSheet->update())
-//                        $done = $employeeTimeSheet->id;
-//                    else
-//                        $done = false;
-//                }
-//
-//                if($done)
-//                {
-//                    $employeeTimeSheet = Employeetimesheet::all()->where("id" , $done)->first() ;
-//                    /**
-//                     * Sending auto generated password through SMS
-//                     */
-//                    if(isset($employeeTimeSheet->getEmployeeFullName()[0]))
-//                        $message = "سلام " .$employeeTimeSheet->user->firstName." عزیز"."\n" ;
-//                    else
-//                        $message = "سلام"."\n" ;
-//
-//                    $todayJalaliDate = $this->convertDate($toDayDate , "toJalali");
-//                    $todayJalaliDate = explode("/" , $todayJalaliDate);
-//                    $jalaliYear = $todayJalaliDate[0];
-//                    $jalaliMonth = $this->convertToJalaliMonth($todayJalaliDate[1]);
-//                    $jalaliDay = $todayJalaliDate[2];
-//                    $jalaliYear = substr( $jalaliYear, -2);
-//                    $todayJalaliDateCaption = "امروز: ";
-//                    $todayJalaliDateCaption = $jalaliDay . " ". $jalaliMonth. " ".$jalaliYear ;
-//                    $message .= $todayJalaliDateCaption."\n" ;
-//                    $persianShiftTime = $employeeTimeSheet->obtainShiftTime("PERSIAN_FORMAT") ;
-//
-//                    if($persianShiftTime !== 0)
-//                    {
-//                        $message .= "موظفی: ". $persianShiftTime."\n";
-//                        $realWorkTime = $employeeTimeSheet->obtainRealWorkTime("IN_SECONDS") ;
-//                        if( $realWorkTime !== false)
-//                            if($realWorkTime == 0)
-//                                $message .= "مرخصی: ".$persianShiftTime ;
-//                            else
-//                                $message .= "اضافه کاری: ".$employeeTimeSheet->obtainWorkAndShiftDiff("HOUR_FORMAT")." منفی" ;
-//                        else
-//                            $message .= "خطا" ;
-//
-//                        if(isset($employee->mobile[0]))
-//                        {
-//                            $smsInfo = array();
-//                            $smsInfo["to"] = array(ltrim($employee->mobile, '0'));
-//                            $smsInfo["from"] = $smsNumber;
-//                            $smsInfo["message"] = $message ;
-//                            $response = $this->medianaSendSMS($smsInfo);
-//                        }else{
-//                            $smsInfo = array();
-//                            $smsInfo["to"] = array(ltrim("09190195476", '0'));
-//                            $smsInfo["from"] = $smsNumber;
-//                            $smsInfo["message"] = $message ;
-//                            $response = $this->medianaSendSMS($smsInfo);
-//                        }
-//                    }
-//                }
-//            }
-//
-//        })
-//            ->dailyAt('22:30')
-//            ->timezone('Asia/Tehran') ;
+        $smsNumber = config("constants.SMS_PROVIDER_DEFAULT_NUMBER");
+        $schedule->call(function () use( $smsNumber ){
+            $dayOfWeekJalali = $this->convertToJalaliDay(Carbon::today('Asia/Tehran')->format('l')) ;
+            $toDayDate = Carbon::today('Asia/Tehran')->format("Y-m-d") ;
+            $employees = User::whereHas("roles" , function ($q){
+                $q->where("name" , Config::get("constants.EMPLOYEE_ROLE")) ;
+            })->get();
+            foreach ($employees as $employee)
+            {
+                $employeeSchedule = Employeeschedule::where("user_id", $employee->id)->where("day" , $dayOfWeekJalali)->get()->first();
+                $employeeTimeSheet = Employeetimesheet::where("user_id" , $employee->id)->where("date" , $toDayDate)->get();
+                $done = false ;
+                if($employeeTimeSheet->isEmpty()){
+                    $newEmplployeeTimeSheet = new Employeetimesheet();
+
+                    $newEmplployeeTimeSheet->date = $toDayDate;
+                    $newEmplployeeTimeSheet->user_id = $employee->id ;
+                    if(isset($employeeSchedule))
+                    {
+                        $newEmplployeeTimeSheet->userBeginTime = $employeeSchedule->getOriginal("beginTime");
+                        $newEmplployeeTimeSheet->userFinishTime = $employeeSchedule->getOriginal("finishTime");
+                        $newEmplployeeTimeSheet->allowedLunchBreakInSec = $employeeSchedule->getOriginal("lunchBreakInSeconds");
+                    }
+                    $newEmplployeeTimeSheet->clockIn = "00:00:00";
+                    $newEmplployeeTimeSheet->beginLunchBreak = "00:00:00";
+                    $newEmplployeeTimeSheet->finishLunchBreak = "00:00:00";
+                    $newEmplployeeTimeSheet->clockOut = "00:00:00";
+                    $newEmplployeeTimeSheet->breakDurationInSeconds = 0 ;
+                    $newEmplployeeTimeSheet->managerComment = "ثبت توسط سیستم : مرخصی یا تعطیلی غیر رسمی";
+                    $newEmplployeeTimeSheet->employeeComment = null;
+                    $newEmplployeeTimeSheet->timeSheetLock = 1;
+                    $newEmplployeeTimeSheet->isPaid = 1;
+                    $newEmplployeeTimeSheet->workdaytype_id = 1;
+
+                    if($newEmplployeeTimeSheet->save())
+                        $done = $newEmplployeeTimeSheet->id;
+                    else
+                        $done = false;
+                }elseif(!$employeeTimeSheet->first()->getOriginal("timeSheetLock")){
+                    $employeeTimeSheet = $employeeTimeSheet->first();
+                    if(strcmp($employeeTimeSheet->clockIn , "00:00:00") == 0 )
+                    {
+                        if(strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") != 0 )
+                            $employeeTimeSheet->clockIn = $employeeTimeSheet->beginLunchBreak ;
+                        elseif(strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") != 0 )
+                            $employeeTimeSheet->clockIn = $employeeTimeSheet->finishLunchBreak ;
+                        elseif(strcmp($employeeTimeSheet->clockOut , "00:00:00") != 0 )
+                            $employeeTimeSheet->clockIn = $employeeTimeSheet->clockOut ;
+                    }
+                    if(strcmp($employeeTimeSheet->clockOut , "00:00:00") == 0 )
+                    {
+                        if(strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") != 0 )
+                            $employeeTimeSheet->clockOut = $employeeTimeSheet->finishLunchBreak ;
+                        elseif(strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") != 0 )
+                            $employeeTimeSheet->clockOut = $employeeTimeSheet->beginLunchBreak ;
+                        elseif(strcmp($employeeTimeSheet->clockIn , "00:00:00") != 0 )
+                            $employeeTimeSheet->clockOut = $employeeTimeSheet->clockIn ;
+                    }
+
+                    if(strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") == 0 &&
+                        strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") != 0)
+                    {
+                        if(strcmp($employeeTimeSheet->clockIn , "00:00:00") != 0 )
+                            $employeeTimeSheet->beginLunchBreak = $employeeTimeSheet->clockIn ;
+                    }
+
+                    if(strcmp($employeeTimeSheet->finishLunchBreak , "00:00:00") == 0 &&
+                        strcmp($employeeTimeSheet->beginLunchBreak , "00:00:00") != 0)
+                    {
+                        if(strcmp($employeeTimeSheet->clockOut , "00:00:00") != 0 )
+                            $employeeTimeSheet->finishLunchBreak = $employeeTimeSheet->clockOut ;
+                    }
+
+                    $employeeTimeSheet->managerComment = $employeeTimeSheet->managerComment." ثبت توسط سیستم : مرخصی یا تعطیلی غیر رسمی";
+                    $employeeTimeSheet->timeSheetLock = 1;
+
+                    if($employeeTimeSheet->update())
+                        $done = $employeeTimeSheet->id;
+                    else
+                        $done = false;
+                }
+
+                if($done)
+                {
+                    $employeeTimeSheet = Employeetimesheet::all()->where("id" , $done)->first() ;
+                    /**
+                     * Sending auto generated password through SMS
+                     */
+                    if(isset($employeeTimeSheet->getEmployeeFullName()[0]))
+                        $message = "سلام " .$employeeTimeSheet->user->firstName." عزیز"."\n" ;
+                    else
+                        $message = "سلام"."\n" ;
+
+                    $todayJalaliDate = $this->convertDate($toDayDate , "toJalali");
+                    $todayJalaliDate = explode("/" , $todayJalaliDate);
+                    $jalaliYear = $todayJalaliDate[0];
+                    $jalaliMonth = $this->convertToJalaliMonth($todayJalaliDate[1]);
+                    $jalaliDay = $todayJalaliDate[2];
+                    $jalaliYear = substr( $jalaliYear, -2);
+                    $todayJalaliDateCaption = "امروز: ";
+                    $todayJalaliDateCaption = $jalaliDay . " ". $jalaliMonth. " ".$jalaliYear ;
+                    $message .= $todayJalaliDateCaption."\n" ;
+                    $persianShiftTime = $employeeTimeSheet->obtainShiftTime("PERSIAN_FORMAT") ;
+
+                    if($persianShiftTime !== 0)
+                    {
+                        $message .= "موظفی: ". $persianShiftTime."\n";
+                        $realWorkTime = $employeeTimeSheet->obtainRealWorkTime("IN_SECONDS") ;
+                        if( $realWorkTime !== false)
+                            if($realWorkTime == 0)
+                                $message .= "مرخصی: ".$persianShiftTime ;
+                            else
+                                $message .= "اضافه کاری: ".$employeeTimeSheet->obtainWorkAndShiftDiff("HOUR_FORMAT")." منفی" ;
+                        else
+                            $message .= "خطا" ;
+
+                        if(isset($employee->mobile[0]))
+                        {
+                            $smsInfo = array();
+                            $smsInfo["to"] = array(ltrim($employee->mobile, '0'));
+                            $smsInfo["from"] = $smsNumber;
+                            $smsInfo["message"] = $message ;
+                            $response = $this->medianaSendSMS($smsInfo);
+                        }else{
+                            $smsInfo = array();
+                            $smsInfo["to"] = array(ltrim("09190195476", '0'));
+                            $smsInfo["from"] = $smsNumber;
+                            $smsInfo["message"] = $message ;
+                            $response = $this->medianaSendSMS($smsInfo);
+                        }
+                    }
+                }
+            }
+
+        })
+            ->dailyAt('22:30')
+            ->timezone('Asia/Tehran') ;
         $schedule->command('backup:mysql-dump')
             ->timezone('Asia/Tehran')
             ->dailyAt('04:30');
