6cb7093e53cd9b237b312a05c0a830c7fe471c55
mohamamad <mohamad.shahrokhi@gmail.com>
2018-08-07 12:22:06 +0430

    Asiatech free ADSL: Creating migrations see also #863

--- database/migrations/2018_08_07_072837_create_productvouchers_table.php
+++ database/migrations/2018_08_07_072837_create_productvouchers_table.php
@@ -19,7 +19,7 @@ class CreateProductvouchersTable extends Migration
             $table->unsignedInteger('product_id')->nullable()->comment('آی دی محصول صاحب وچر');
             $table->string('code')->nullable()->comment('پین کد وچر');
             $table->timestamp('expirationdatetime')->nullable()->comment('زمان انقضای این پین کد');
-            $table->tinyInteger('enable')->nullable()->comment('فعال یا غیرفعال بودن پین');
+            $table->tinyInteger('enable')->default(1)->comment('فعال یا غیرفعال بودن پین');
             $table->string('description')->nullable()->comment('توضیح این پین کد');
             $table->timestamps();
             $table->softDeletes();
