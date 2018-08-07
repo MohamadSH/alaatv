3287bec55793f4632858fcb2f61753b18a1e5ee6
mohamamad <mohamad.shahrokhi@gmail.com>
2018-08-07 12:22:46 +0430

    Asiatech free ADSL: Creating models fix #864

---
+++ app/Productvoucher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Productvoucher extends Model
+{
+    use SoftDeletes;
+    /**      * The attributes that should be mutated to dates.        */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'product_id',
+        'code',
+        'expirationdatetime',
+        'enable',
+        'description',
+    ];
+
+    public function products()
+    {
+        return $this->belongsTo('App\Product');
+    }
+}
