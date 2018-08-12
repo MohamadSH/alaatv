b8419d92500b0b4a5f4f83a9f3ea4dec36bf6d6e
sanatisharif <foratmail@gmail.com>
2018-08-12 19:06:00 +0430

    fix bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -892,8 +892,8 @@ class HomeController extends Controller
 //                'https://cdn.sanatisharif.ir/upload/ads/SMALL-SLIDE-2.jpg' => 'https://sanatisharif.ir/landing/4',
 //                'https://cdn.sanatisharif.ir/upload/ads/SMALL-SLIDE-3.jpg' => 'https://sanatisharif.ir/landing/4',
 //            ];
-            $ads1 = null;
-            $ads2 = null;
+            $ads1 = [];
+            $ads2 = [];
             return view("pages.search" , compact("items" ,"itemTypes" ,"tagArray" , "extraTagArray",
                 "majors" , "grades"  , "defaultLesson" , "sideBarMode" , "majorLesson" , "lessonTeacher" , "defaultTeacher"
                 , "ads1" , "ads2" , 'tagInput' ,'defaultGrade' , 'defaultMajor' ));
