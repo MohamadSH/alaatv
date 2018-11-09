df75546c7073bb26c0bd5e998c22477bb2d52533
sanatisharif <foratmail@gmail.com>
2018-11-09 19:04:14 +0330

    upgrade laravel@5.6 to 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+/*mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');*/
 
 mix.styles([
     'public/assets/extra/fonts/IRANSans/css/fontiran.css',
