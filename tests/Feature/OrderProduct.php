366e3fd75c2910678d92daebf7525711aee993ba
kerasus <aliesmaeeli20@gmail.com>
2019-01-28 18:01:17 +0330

    unit test payment and orderproduct

--- tests/Feature/OrderProduct.php
+++ tests/Feature/OrderProduct.php
@@ -6,9 +6,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class paymentTest extends TestCase
+class OrderProduct extends TestCase
 {
-    public function testRedirect() {
+    public function addToOrder() {
 
     }
 }
