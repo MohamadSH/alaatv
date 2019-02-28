23072c8c3261a3397eb730b8aa8aef692ce15abd
mohamamad <mohamad.shahrokhi@gmail.com>
2019-02-28 16:51:51 +0330

    User update and store refactor

--- app/Http/Requests/EditUserRequest.php
+++ app/Http/Requests/EditUserRequest.php
@@ -13,48 +13,35 @@ class EditUserRequest extends FormRequest
     use CharacterCommon;
     use RequestCommon ;
 
-    const USER_UPDATE_TYPE_TOTAL    = "total" ;
-    const USER_UPDATE_TYPE_PROFILE  = "profile" ;
-    const USER_UPDATE_TYPE_ATLOGIN  = "atLogin" ;
-    const USER_UPDATE_TYPE_PASSWORD = "password" ;
-    const USER_UPDATE_TYPE_PHOTO    = "photo" ;
+    const USER_UPDATE_TYPE_TOTAL    = 'total' ;
+    const USER_UPDATE_TYPE_PROFILE  = 'profile' ;
+    const USER_UPDATE_TYPE_ATLOGIN  = 'atLogin' ;
+    const USER_UPDATE_TYPE_PASSWORD = 'password' ;
+    const USER_UPDATE_TYPE_PHOTO    = 'photo' ;
+
+    const PHOTO_RULE                = '|image|mimes:jpeg,jpg,png|max:512';
 
     /**
      * Determine if the user is authorized to make this request.
      *
+     * @param Request $request
      * @return bool
      */
-    public function authorize()
+    public function authorize(Request $request)
     {
-        return true;
-
-        //ToDo : Has put at the begining of UserController@update . Needs to be refactored
-
-        /*$user = $this->user();
-        $updateType = $this->request->get("updateType");
         $authorized = true;
-        switch ($updateType)
-        {
-            case self::USER_UPDATE_TYPE_TOTAL :
-                if (!$user->can(config('constants.EDIT_USER_ACCESS')))
-                    $authorized = false;
-                break;
-            case self::USER_UPDATE_TYPE_PASSWORD :
-            case self::USER_UPDATE_TYPE_ATLOGIN :
-            case self::USER_UPDATE_TYPE_PHOTO :
-            case self::USER_UPDATE_TYPE_PROFILE :
-                $isRequestedUserAndAuthSame = $this->hasRequestAuthUser($user , $this);
-                if(!$isRequestedUserAndAuthSame)
-                    $authorized = false;
 
-                if($user->isUserProfileLocked())
-                    $authorized = false;
-                break;
-            default:
-                break;
+        $authenticatedUser = $request->user();
+        $userId = $request->segment(2);
+        if($userId==null) {
+            $user = $authenticatedUser;
+            if($user->isUserProfileLocked())
+                $authorized = false;
+        } elseif (!$authenticatedUser->can(config('constants.EDIT_USER_ACCESS'))) {
+            $authorized = false;
         }
 
-        return $authorized;*/
+        return $authorized;
     }
 
     /**
@@ -64,9 +51,9 @@ class EditUserRequest extends FormRequest
      */
     public function rules()
     {
-        $userId = $this->request->get("id");
+        $userId = $this->request->get('id');
 
-        $updateType = $this->request->get("updateType" , self::USER_UPDATE_TYPE_TOTAL);
+        $updateType = $this->request->get('updateType' , self::USER_UPDATE_TYPE_TOTAL);
         switch ($updateType)
         {
             case self::USER_UPDATE_TYPE_TOTAL :
@@ -79,7 +66,7 @@ class EditUserRequest extends FormRequest
                         Rule::phone()->mobile()->country('AUTO,IR'),
                         Rule::unique('users')
                             ->where(function ($query) use ($userId){
-                                $query->where('nationalCode', $this->request->get("nationalCode"))
+                                $query->where('nationalCode', $this->request->get('nationalCode'))
                                     ->where('id','<>',$userId)
                                     ->where('deleted_at', null);
                             }),
@@ -90,16 +77,16 @@ class EditUserRequest extends FormRequest
                         'validate:nationalCode',
                         Rule::unique('users')
                             ->where(function ($query) use($userId) {
-                                $query->where('mobile', $this->request->get("mobile"))
+                                $query->where('mobile', $this->request->get('mobile'))
                                     ->where('id','<>',$userId)
                                     ->where('deleted_at', null);
                             }),
                     ],
+                    'password'      => 'required|confirmed|min:6',
                     'userstatus_id' => 'required|exists:userstatuses,id',
-                    'photo'         => 'sometimes|nullable|image|mimes:jpeg,jpg,png|max:512',
+                    'photo'         => 'sometimes|nullable'.self::PHOTO_RULE,
                     'postalCode'    => 'sometimes|nullable|numeric',
                     'email'         => 'sometimes|nullable|email',
-                    'password'      => 'sometimes|nullable|confirmed|min:6',
                     'major_id'      => 'sometimes|nullable|exists:majors,id',
                     'gender_id'     => 'sometimes|nullable|exists:genders,id',
                     'techCode'      => 'sometimes|nullable|alpha_num|max:5|min:5|unique:users,techCode,' . $userId . ',id',
@@ -109,13 +96,12 @@ class EditUserRequest extends FormRequest
                 $rules = [
                     'postalCode' => 'sometimes|nullable|numeric',
                     'email'      => 'sometimes|nullable|email',
-                    'photo'      => 'sometimes|nullable|image|mimes:jpeg,jpg,png|max:800',
-                    //ToDo: it needs to be required but will conflict with profile update
+                    'photo'      => 'sometimes|nullable'.self::PHOTO_RULE,
                 ];
                 break;
             case self::USER_UPDATE_TYPE_PHOTO :
                 $rules = [
-                    'photo'      => 'required|image|mimes:jpeg,jpg,png|max:800',
+                    'photo'      => 'required'.self::PHOTO_RULE,
                 ];
                 break;
             case self::USER_UPDATE_TYPE_ATLOGIN :
@@ -124,16 +110,15 @@ class EditUserRequest extends FormRequest
                 //ToDo : Could be refactored to be more dynamic
                 $rules = [];
                 foreach ($afterLoginFields as $afterLoginField) {
-                    if (strcmp($afterLoginField, "email") == 0)
-                        $rules[$afterLoginField] = "required|email";
-                    else if (strcmp($afterLoginField, "photo") == 0)
-                        $rules[$afterLoginField] = "required|image|mimes:jpeg,jpg,png|max:512";
-                    else if (strcmp($afterLoginField, "major_id") == 0)
-                        $rules[$afterLoginField] = "required|exists:majors,id";
-                    else if (strcmp($afterLoginField, "gender_id") == 0)
-                        $rules[$afterLoginField] = "required|exists:genders,id";
+                    $rule = 'required';
+                    if (strcmp($afterLoginField, 'email') == 0)
+                        $rule .= '|email';
+                    else if (strcmp($afterLoginField, 'photo') == 0)
+                        $rule .= self::PHOTO_RULE;
                     else
-                        $rules[$afterLoginField] = "required|max:255";
+                        $rule .= '|max:255';
+
+                    $rules[$afterLoginField] = $rule;
                 }
                 break;
             case self::USER_UPDATE_TYPE_PASSWORD :
@@ -159,24 +144,24 @@ class EditUserRequest extends FormRequest
     private function replaceNumbers()
     {
         $input = $this->request->all();
-        if (isset($input["mobile"])) {
-            $input["mobile"] = preg_replace('/\s+/', '', $input["mobile"]);
-            $input["mobile"] = $this->convertToEnglish($input["mobile"]);
+        if (isset($input['mobile'])) {
+            $input['mobile'] = preg_replace('/\s+/', '', $input['mobile']);
+            $input['mobile'] = $this->convertToEnglish($input['mobile']);
         }
-        if (isset($input["postalCode"])) {
-            $input["postalCode"] = preg_replace('/\s+/', '', $input["postalCode"]);
-            $input["postalCode"] = $this->convertToEnglish($input["postalCode"]);
+        if (isset($input['postalCode'])) {
+            $input['postalCode'] = preg_replace('/\s+/', '', $input['postalCode']);
+            $input['postalCode'] = $this->convertToEnglish($input['postalCode']);
         }
-        if (isset($input["nationalCode"])) {
-            $input["nationalCode"] = preg_replace('/\s+/', '', $input["nationalCode"]);
-            $input["nationalCode"] = $this->convertToEnglish($input["nationalCode"]);
+        if (isset($input['nationalCode'])) {
+            $input['nationalCode'] = preg_replace('/\s+/', '', $input['nationalCode']);
+            $input['nationalCode'] = $this->convertToEnglish($input['nationalCode']);
         }
-        if (isset($input["password"])) {
-            $input["password"] = $this->convertToEnglish($input["password"]);
+        if (isset($input['password'])) {
+            $input['password'] = $this->convertToEnglish($input['password']);
         }
-        if (isset($input["email"])) {
-            $input["email"] = preg_replace('/\s+/', '', $input["email"]);
-            $input["email"] = $this->convertToEnglish($input["email"]);
+        if (isset($input['email'])) {
+            $input['email'] = preg_replace('/\s+/', '', $input['email']);
+            $input['email'] = $this->convertToEnglish($input['email']);
         }
         $this->replace($input);
     }
