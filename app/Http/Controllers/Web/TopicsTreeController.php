c4d5488735e5f596b8919b1d9d12c1dfa0cbed6c
kerasus <aliesmaeeli20@gmail.com>
2019-04-28 11:05:36 +0430

    git ignore

--- app/Http/Controllers/Web/TopicsTreeController.php
+++ app/Http/Controllers/Web/TopicsTreeController.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Web;
 
-use App\Console\Commands\CategoryTree\TotalTree;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Console\Commands\CategoryTree\TotalTree;
 
 class TopicsTreeController extends Controller
 {
     /**
-     * @param  Request  $request
-     *
+     * @param Request $request
      * @return
      */
     public function lernitoTree(Request $request)
@@ -18,10 +18,10 @@ class TopicsTreeController extends Controller
         $htmlPrint = $TotalTree->getLernitoTreeHtmlPrint();
         return view('admin.topicsTree.index', compact('htmlPrint'));
     }
-    
+
     public function getTreeInPHPArrayString(Request $request, $lnid)
     {
-        $TotalTree    = new TotalTree();
+        $TotalTree = new TotalTree();
         $stringFormat = $TotalTree->getTreeNodeByIdInHtmlString($lnid);
         return $stringFormat;
     }
