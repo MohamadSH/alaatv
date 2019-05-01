e02ab7471ba2b121e046fed416106d9b81d5f97d
sohrabafard <sohrab.a.fard@gmail.com>
2019-05-01 15:09:47 +0430

    Hide product_set pivot in json seed more block

--- app/Product.php
+++ app/Product.php
@@ -228,6 +228,7 @@ class Product extends BaseModel implements Advertisable, Taggable, SeoInterface,
         'created_at',
         'attributevalues',
         'grand',
+        'productSet',
     ];
     
     /**
