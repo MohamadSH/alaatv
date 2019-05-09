855759f2a573f3ddc7a5e12aeed33d036ba3a1de
mohamamad <mohamad.shahrokhi@gmail.com>
2019-05-09 21:29:32 +0430

    Checkout payment bug fix

--- app/PaymentModule/Repositories/OrdersRepo.php
+++ app/PaymentModule/Repositories/OrdersRepo.php
@@ -3,6 +3,7 @@
 namespace App\PaymentModule\Repositories;
 
 use App\Order;
+use Carbon\Carbon;
 
 class OrdersRepo
 {
@@ -11,6 +12,7 @@ class OrdersRepo
         return Order::where('id', $id)
             ->update([
                 'orderstatus_id' => config('constants.ORDER_STATUS_CLOSED'),
+                'completed_at'   => Carbon::now()
             ]);
     }
 }
