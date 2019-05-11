fae2ecbdba15f6602a1be4f55d6d3993c8cc09c9
mohamamad <mohamad.shahrokhi@gmail.com>
2019-05-11 18:34:20 +0430

    checkout payment bug
    fix #1508

--- app/Http/Controllers/Web/OrderController.php
+++ app/Http/Controllers/Web/OrderController.php
@@ -1001,7 +1001,12 @@ class OrderController extends Controller
             
             $invoiceInfo = $invoiceGenerator->generateOrderInvoice($order);
             //ToDo : no need to orderproducts
-            
+
+            $orderproducts = $invoiceInfo['items'];
+            /** @var OrderproductCollection $orderproducts */
+            if($orderproducts->isEmpty())
+                return redirect()->route("checkoutReview");
+
             $response = response([
                 'price'                       => $invoiceInfo['price'],
                 'credit'                      => $credit,
