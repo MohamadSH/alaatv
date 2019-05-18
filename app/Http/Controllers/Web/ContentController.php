4ca43ec0e2476dd3a9c2da1ba1e93c44820a2f74
mohamamad <mohamad.shahrokhi@gmail.com>
2019-05-18 18:23:39 +0430

    Insert content bug

--- app/Http/Controllers/Web/ContentController.php
+++ app/Http/Controllers/Web/ContentController.php
@@ -479,7 +479,8 @@ class ContentController extends Controller
             }
 
             Cache::tags('content')->flush();
-            session()->put('success', 'تغییر نام با موفقیت انجام شد');
+//            session()->put('success', 'تغییر نام با موفقیت انجام شد');
+            session()->put('error', 'این قابلیت در حال حاضر غیر فعال می باشد');
             return redirect()->back();
         }
 
@@ -491,8 +492,8 @@ class ContentController extends Controller
         $dateNow        = Carbon::now();
         
         $contentset  = Contentset::FindOrFail($contentset_id);
-        $lastContent = $contentset->getLastContent();
-        
+        $lastContent = $contentset->contents2()->where("contenttype_id" , $contenttype_id)->get()->sortByDesc("order")->first();
+
         if (!isset($lastContent)) {
             session()->put('error', trans('content.No previous content found'));
 
@@ -509,8 +510,6 @@ class ContentController extends Controller
             $order = $lastOrder + 1;
         }
 
-        $newContent->contenttype_id = $contenttype_id;
-        $newContent->contentset_id = $contentset->id;
         $newContent->name = $name;
         $newContent->order = $order;
         $newContent->description = null;
