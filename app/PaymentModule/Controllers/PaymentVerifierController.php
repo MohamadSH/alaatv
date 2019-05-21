7ef267ecdc564d10789ca0cc420ca7a85c21e6c1
mohamamad <mohamad.shahrokhi@gmail.com>
2019-05-21 11:12:50 +0430

    Adding to payment verifier message

--- app/PaymentModule/Controllers/PaymentVerifierController.php
+++ app/PaymentModule/Controllers/PaymentVerifierController.php
@@ -49,7 +49,9 @@ class PaymentVerifierController extends Controller
                 $verificationResult->getCardPanMask()
             );
             $this->handleOrderSuccessPayment($transaction->order);
-
+            $assetLink = '<a href="'.route('user.asset').'"></a>';
+            $responseMessages[]= 'برای دانلود محصولاتی که خریده اید به صفحه روبرو بروید: '.$assetLink;
+            $responseMessages[]= 'توجه کنید که محصولات پیش خرید شده در تاریخ مقرر شده برای دانلود قرار داده می شوند';
         } else {
             $this->handleOrderCanceledPayment($transaction->order);
             $this->handleOrderCanceledTransaction($transaction);
