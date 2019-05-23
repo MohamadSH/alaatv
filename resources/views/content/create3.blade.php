1d4aca8d8cb08d26451f916f141d7c5ef3e07bde
kerasus <aliesmaeeli20@gmail.com>
2019-05-23 13:08:23 +0430

    see also #1661

--- resources/views/content/create3.blade.php
+++ resources/views/content/create3.blade.php
@@ -1,6 +1,8 @@
 @extends("app",["pageName"=>"admin"])
 
-@section("headPageLevelPlugin")
+@section('page-css')
+    <link href = "/acm/AlaatvCustomFiles/components/alaa_old/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css" rel = "stylesheet" type = "text/css"/>
+    <link href="/acm/extra/persian-datepicker/dist/css/persian-datepicker-0.4.5.min.css" rel="stylesheet" type="text/css"/>
 @endsection
 
 @section('pageBar')
@@ -38,23 +40,21 @@
                 </div>
                 <div class="m-portlet__body">
                     {!! Form::open(['method' => 'POST', 'action' => 'Web\ContentController@store', 'id' => 'frmCreateNewContent']) !!}
-                    <div class="row">
-                        <div class="col-md-8 mx-auto">
-                            <div class="form-group m-form__group">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <button class="btn btn-success btnShowLastContentData" type="button">
-                                            نمایش اطلاعات آخرین محتوا
-                                        </button>
+                        <div class="row">
+                            <div class="col-md-8 mx-auto">
+                                <div class="form-group m-form__group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <button class="btn btn-success btnShowLastContentData" type="button">
+                                                نمایش اطلاعات آخرین محتوا
+                                            </button>
+                                        </div>
+                                        <input type="text" name="contentset_id" id="setId" class="form-control m-input m-input--air" placeholder="شماره درس" dir="ltr">
                                     </div>
-                                    <input type="text" name="contentset_id" id="setId" class="form-control m-input m-input--air" placeholder="شماره درس" dir="ltr">
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    
-                    <hr>
-
+                        <hr>
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
@@ -74,14 +74,13 @@
                                         </div>
                                     </div>
                                 </div>
-            
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <div class="row">
                                         <label class="col-md-4 control-label" for="contenttypes">نوع محتوا</label>
                                         <div class="col-md-8">
-                                            {!! Form::select('contenttype_id', $contenttypes, null,['class' => 'form-control m-input m-input--air', 'id' => 'contenttypes']) !!}
+                                            {!! Form::select('contenttype_id', $contenttypes, (isset($lastContent)?$lastContent->contenttype_id:null),['class' => 'form-control m-input m-input--air', 'id' => 'contenttypes']) !!}
                                         </div>
                                     </div>
                                 </div>
@@ -90,113 +89,140 @@
                                     <div class="row">
                                         <label class="col-md-4 control-label" for="contenttypes">ترتیب</label>
                                         <div class="col-md-8">
-                                            {!! Form::text('order', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'ترتیب' , 'id'=>'order', 'dir'=>'ltr']) !!}
+                                            {!! Form::text('order', (isset($lastContent)?($lastContent->order+1):null), ['class' => 'form-control m-input m-input--air', 'placeholder'=>'ترتیب' , 'id'=>'order', 'dir'=>'ltr']) !!}
                                         </div>
                                     </div>
-                                    {!! Form::text('order', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'ترتیب' , 'id'=>'order' , 'disabled', 'dir'=>'ltr']) !!}
                                 </div>
                             </div>
-                            <span class = "help-block">
-                                    <strong></strong>
-                                </span>
                         </div>
-                        <div class = "col-md-6">
-                            {!! Form::text('name', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'عنوان']) !!}
-                            <span class = "help-block">
-                                    <strong></strong>
-                                </span>
-                        </div>
-                        <div class = "col-md-6" id = "fileNameSection" style = "border: solid gray 0.5px; padding: 10px;">
-                            <div class = "col-md-12">
-{{--                                <div class = "mt-radio-list">--}}
-{{--                                    <label class = "mt-radio mt-radio-outline"> درج با اسم فایل--}}
-{{--                                        <input class = "modeRadio" type = "radio" value = "filename" name = "mode" checked/>--}}
-{{--                                        <span></span>--}}
-{{--                                    </label>--}}
-{{--                                </div>--}}
-                                {!! Form::text('fileName', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'نام کامل فایل', 'dir'=>'ltr']) !!}
-                                <span class = "help-block">
-                                        <strong></strong>
-                                    </span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-md-2 control-label" for="name">نام :
+                                            <span class="required"> * </span>
+                                        </label>
+                                        <div class="col-md-10">
+                                            {!! Form::text('name', (isset($lastContent)?$lastContent->name:null), ['class' => 'form-control', 'id' => 'name', 'maxlength'=>'100' ]) !!}
+                                            <span class="help-block">
+                                                   <strong></strong>
+                                               </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-md-2 control-label" for="description">توضیح:
+                                        </label>
+                                        <div class="col-md-10">
+                                            {!! Form::textarea('description', (isset($lastContent)?$lastContent->description:null), ['class' => 'form-control', 'id' => 'descriptionSummerNote', 'rows' => '5' ]) !!}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-md-2 control-label" for="tags">
+                                            تگ ها :
+                                        </label>
+                                        <div class="col-md-10">
+                                            <input name="tags" type="text" id="contentTags" class="form-control input-large" value="{{ (isset($lastContent)?(implode(',',$lastContent->tags->tags)):'') }}" data-role="tagsinput">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-{{--                        <div class = "col-md-6" id = "fileLinkSection" style = "border: solid gray 0.5px; padding: 10px;">--}}
-{{--                            <div class = "col-md-12">--}}
-{{--                                <div class = "mt-radio-list">--}}
-{{--                                    <label class = "mt-radio mt-radio-outline"> درج با لینک--}}
-{{--                                        <input class = "modeRadio" type = "radio" value = "filelink" name = "mode"/>--}}
-{{--                                        <span></span>--}}
-{{--                                    </label>--}}
-{{--                                </div>--}}
-{{--                                {!! Form::text('hd', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'لینک کیفیت HD', 'dir'=>'ltr']) !!}--}}
-{{--                                <span class = "help-block">--}}
-{{--                                        <strong></strong>--}}
-{{--                                    </span>--}}
-{{--                            </div>--}}
-{{--                            <div class = "col-md-12">--}}
-{{--                                {!! Form::text('hq', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'لینک کیفیت hq', 'dir'=>'ltr']) !!}--}}
-{{--                                <span class = "help-block">--}}
-{{--                                        <strong></strong>--}}
-{{--                                    </span>--}}
-{{--                            </div>--}}
-{{--                            <div class = "col-md-12">--}}
-{{--                                {!! Form::text('240p', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'لینک کیفیت 240p', 'dir'=>'ltr']) !!}--}}
-{{--                                <span class = "help-block">--}}
-{{--                                        <strong></strong>--}}
-{{--                                    </span>--}}
-{{--                            </div>--}}
-{{--                            <div class = "col-md-12">--}}
-{{--                                {!! Form::text('thumbnail', null, ['class' => 'form-control m-input m-input--air', 'placeholder'=>'لینک تامبنیل', 'dir'=>'ltr']) !!}--}}
-{{--                                <span class = "help-block">--}}
-{{--                                        <strong></strong>--}}
-{{--                                    </span>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
                         <div class = "col-md-12 text-center m--margin-top-10">
                             <input type = "submit" class = "btn btn-lg m-btn--pill m-btn--air m-btn m-btn--gradient-from-success m-btn--gradient-to-accent" value = "درج">
                         </div>
-                    </div>
                     {!! Form::close() !!}
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
 
-@section("footerPageLevelPlugin")
-@endsection
-
-@section("footerPageLevelScript")
-@endsection
-
-
 @section('page-js')
+    <script src="/acm/AlaatvCustomFiles/components/alaa_old/plugins/bootstrap-tagsinput/bootstrap-tagsinput.min.js" type="text/javascript"></script>
+    <script src="/acm/extra/persian-datepicker/lib/persian-date.js" type="text/javascript"></script>
+    <script src="/acm/extra/persian-datepicker/dist/js/persian-datepicker-0.4.5.min.js" type="text/javascript"></script>
+    
     <script>
 
+
+        $("input[data-role=tagsinput]").tagsinput({
+            tagClass: 'm-badge m-badge--info m-badge--wide m-badge--rounded'
+        });
+
         $(document).ready(function () {
-            var height = $("#fileLinkSection").height();
-            $("#fileNameSection").css("min-height", height + 20);
-            $("#fileLinkSection :input").not("[name=mode]").attr("disabled", true);
+
+            $('#descriptionSummerNote').summernote({height: 300});
+
+            $("#validSinceDate").persianDatepicker({
+                altField: '#validSinceDateAlt',
+                altFormat: "YYYY MM DD",
+                observer: true,
+                format: 'YYYY/MM/DD',
+                timePicker: {
+                    enabled: true,
+                    meridiem: {
+                        enabled: false
+                    }
+                },
+                altFieldFormatter: function (unixDate) {
+                    let targetDatetime = new Date(unixDate);
+                    return targetDatetime.getFullYear() + "-" + (targetDatetime.getMonth() + 1) + "-" + targetDatetime.getDate() + ' ' + targetDatetime.getHours() + ':' + targetDatetime.getMinutes() + ':' + targetDatetime.getSeconds();
+                }
+            });
+
         });
 
-        $(document).on("click", "#orderCheckbox", function () {
-            if ($(this).prop("checked")) {
-                $("#order").prop("disabled", false);
-            } else {
-                $("#order").prop("disabled", true);
-            }
+        $(document).on('click', '.btnShowLastContentData', function () {
+            var setId = $('#setId').val();
+            getLasContentOfSet(setId, initLastContentData);
         });
 
-        $(document).on("click", ".modeRadio", function () {
-            var value = $(this).val();
-            if (value == "filename") {
-                $("#fileNameSection :input").not("[name=mode]").attr("disabled", false);
-                $("#fileLinkSection :input").not("[name=mode]").attr("disabled", true);
-            } else if (value == "filelink") {
-                $("#fileNameSection :input").not("[name=mode]").attr("disabled", true);
-                $("#fileLinkSection :input").not("[name=mode]").attr("disabled", false);
-            }
-        });
+
+        function getLasContentOfSet(setId, callback) {
+
+            mApp.block('#frmCreateNewContent', {
+                overlayColor: "#000000",
+                type: "loader",
+                state: "success",
+                message: "کمی صبر کنید..."
+            });
+
+            $.ajax({
+                type: 'GET',
+                url: window.location.origin+window.location.pathname+'?set='+setId,
+                data: {},
+                dataType: 'json',
+                success: function (data) {
+                    if (typeof data.url !== 'undefined') {
+                        callback(data);
+                    } else {
+                        mApp.unblock('#frmCreateNewContent');
+                        toastr.error('خطای سیستمی رخ داده است.');
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    mApp.unblock('#frmCreateNewContent');
+                    toastr.error('خطای سیستمی رخ داده است.');
+                }
+            });
+        }
+
+        function initLastContentData(data) {
+            $('#contenttypes').val(data.contenttype_id);
+            $('#order').val(data.order+1);
+            $('#name').val(data.name);
+            $('#descriptionSummerNote').summernote('code', data.description);
+            $("input[data-role=tagsinput]").tagsinput('destroy');
+            $("input[data-role=tagsinput]").val(data.tags.tags.join());
+            $("input[data-role=tagsinput]").tagsinput({
+                tagClass: 'm-badge m-badge--info m-badge--wide m-badge--rounded'
+            });
+        }
+    
+    
     </script>
 @endsection
