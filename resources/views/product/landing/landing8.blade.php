45af40fe3bf149e3f83b731c9dd57d6c13cb30fa
kerasus <aliesmaeeli20@gmail.com>
2019-05-23 16:36:43 +0430

    -landing 8 update

--- resources/views/product/landing/landing8.blade.php
+++ resources/views/product/landing/landing8.blade.php
@@ -50,101 +50,96 @@
                             </div>
                         </div>
                         <div class="m-widget27__container">
-                            
+                            <style>
+                                .m-portlet--creative .m-portlet__head-caption {
+                                    width: 100%;
+                                    text-align: center;
+                                }
+                                .m-portlet--creative .m-portlet__head {
+                                    height: 0px !important;
+                                }
+                            </style>
                             <div class="container-fluid m--padding-right-40 m--padding-left-40">
                                 <div class="row">
-                                    <div class="col-md-3 order-2 order-sm-2 order-md-1 order-lg-1 sideItems">
-                                        <div class="m-portlet m-portlet--bordered m-portlet--unair">
-                                            <div class="m-portlet__body m--padding-10">
-                                                <div class="m-widget24">
-                                                    <div class="m-widget24__item">
-                                                        <h4 class="m-widget24__title m--font-info">
-                                                            
-                                                        </h4>
-                                                        <span class="m-widget24__stats m--font-info">
-                                                            <img src="/assets/extra/landing1/img/like.png" style="height: 40px;">
-                                                        </span>
-                                                        <br>
-                                                        <span class="m-widget24__desc">
-                                                            همایش طلایی آلاء
-                                                        </span>
-                                                        <div class="m--space-10"></div>
+                                    <div class="col-md-4 order-2 order-sm-2 order-md-1 order-lg-1 sideItems">
+                                        
+                                        <div class="m-portlet m-portlet--creative m-portlet--first m-portlet--bordered-semi">
+                                            <div class="m-portlet__head">
+                                                <div class="m-portlet__head-caption">
+                                                    <div class="m-portlet__head-title">
+                                                        <h2 class="m-portlet__head-label m-portlet__head-label--info">
+                                                            <span>
+                                                                همایش طلایی آلاء
+                                                            </span>
+                                                        </h2>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="m-portlet__body">
+            
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                جمع بندی حداقل 80 درصد مباحث کنکور
+                                                <br>
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                حل تست های فراوان آموزشی و نکته دار
+                                                <br>
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                پیش بینی و بررسی تست های احتمالی کنکور
+                                                <br>
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                یک منبع کامل و مورد اعتماد
+        
+                                            </div>
+                                        </div>
     
-                                                        <span class="m-badge m-badge--danger m-badge--dot"></span>
-                                                        جمع بندی حداقل 80 درصد مباحث کنکور
-                                                        <br>
-                                                        <span class="m-badge m-badge--danger m-badge--dot"></span>
-                                                        حل تست های فراوان آموزشی و نکته دار
-                                                        <br>
-                                                        <span class="m-badge m-badge--danger m-badge--dot"></span>
-                                                        پیش بینی و بررسی تست های احتمالی کنکور
-                                                        <br>
-                                                        <span class="m-badge m-badge--danger m-badge--dot"></span>
-                                                        یک منبع کامل و مورد اعتماد
-                                                        
-                                                        
+                                        <div class="m-portlet m-portlet--creative m-portlet--first m-portlet--bordered-semi m--margin-top-50">
+                                            <div class="m-portlet__head">
+                                                <div class="m-portlet__head-caption">
+                                                    <div class="m-portlet__head-title">
+                                                        <h2 class="m-portlet__head-label m-portlet__head-label--info">
+                                                            <span>
+                                                                برنامه دقیق و منسجم
+                                                            </span>
+                                                        </h2>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <div class="m-portlet__body">
+    
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                همایش طلایی؛ یک نقشه راه مطمئن، از مسیری کوتاه و قابل اعتماد برای موفقیت تا روز کنکور است.
+                                                
+                                            </div>
                                         </div>
-                                        <div class="m-portlet m-portlet--bordered m-portlet--unair">
-                                            <div class="m-portlet__body m--padding-10">
-                                                <div class="m-widget24">
-                                                    <div class="m-widget24__item">
-                                                        <h4 class="m-widget24__title m--font-success">
-                                                            ۱/۳
-                                                        </h4>
-                                                        <span class="m-widget24__stats m--font-success">
-                                                            <i class="flaticon-pie-chart"></i>
-                                                        </span>
-                                                        <br>
-                                                        <span class="m-widget24__desc">
-                                                            جمع بندی یک سوم کنکور
-                                                        </span>
-                                                        <div class="m--space-10"></div>
-                                                        <div class="progress m-progress--sm">
-                                                            <div class="progress-bar m--bg-success" role="progressbar" style="width: 33.33%;" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100"></div>
-                                                        </div>
-                                                        <span class="m-widget24__change">
-                                                            کنکور
-                                                        </span>
-                                                        <span class="m-widget24__number">
-                                                            ۳۳%
-                                                        </span>
+    
+                                        <div class="m-portlet m-portlet--creative m-portlet--first m-portlet--bordered-semi m--margin-top-50">
+                                            <div class="m-portlet__head">
+                                                <div class="m-portlet__head-caption">
+                                                    <div class="m-portlet__head-title">
+                                                        <h2 class="m-portlet__head-label m-portlet__head-label--info">
+                                                            <span>
+                                                                همایش های طلایی آلاء
+                                                            </span>
+                                                        </h2>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="m-portlet m-portlet--bordered m-portlet--unair portlet--lastPart">
-                                            <div class="m-portlet__body m--padding-10">
-                                                <div class="m-widget24">
-                                                    <div class="m-widget24__item">
-                                                        <h4 class="m-widget24__title m--font-warning">
-                                                            اساتید آلاء
-                                                        </h4>
-                                                        <span class="m-widget24__stats m--font-warning">
-                                                            <img src="/assets/extra/landing1/img/teacher-128.png" style="height: 40px;">
-                                                        </span>
-                                                        <br>
-                                                        <span class="m-widget24__desc">
-                                                            اساتیدی که می شناسید
-                                                        </span>
-                                                        <div class="m--space-10"></div>
-                                                        <div class="progress m-progress--sm">
-                                                            <div class="progress-bar m--bg-warning" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                                        </div>
-                                                        <span class="m-widget24__change">
-                                                            همه اساتید
-                                                        </span>
-                                                        <span class="m-widget24__number">
-                                                            100%
-                                                        </span>
-                                                    </div>
-                                                </div>
+                                            <div class="m-portlet__body">
+    
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                هنوز خودتون رو توی بزرگراه رقابت کنکور نمی بینید
+                                                <br>
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                ویا در مسیر هستید اما سرعت مناسبی ندارید
+                                                <br>
+                                                <span class="m-badge m-badge--danger m-badge--dot"></span>
+                                                و یا نیاز به یک جمع بندی تمام کننده دارید
+
                                             </div>
                                         </div>
+                                        
                                     </div>
-                                    <div class="col-md-9 order-1 order-sm-1 order-md-2 order-lg-2">
+                                    <div class="col-md-8 order-1 order-sm-1 order-md-2 order-lg-2">
                                         
                                         <div class="row justify-content-center">
                                             @foreach( $landingProducts as $product)
