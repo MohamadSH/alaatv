2842f0308effb171cfd1e0e78c0f5c2940e7c57f
kerasus <aliesmaeeli20@gmail.com>
2019-06-04 09:58:55 +0430

    certificate UPDATE

--- resources/views/partials/certificates.blade.php
+++ resources/views/partials/certificates.blade.php
@@ -43,36 +43,6 @@
             </div>
         </div>
     </div>
-    
-    <div class="col-md-9 d-none">
-        <div class="m-portlet">
-            <div class="m-portlet__body">
-                <div class="row">
-                    <div class="col-md-3">
-                        <img src="/acm/extra/Alaa-logo.gif" class="img-fluid m--img-centered" alt="فیلم کنکور آلاء"/>
-                    </div>
-                    <div class="col-md-9 my-auto">
-                        <p class="text-justify">
-                            آلاء پنجره ای است رو به دور نمای آموزش کشور که می کوشد با اساتید کار بلد و مخاطبان پر تعداد و متعهد خود آموزش همگانی را در چهار گوشه ی این سرزمین در دسترس فرزندان ایران قرار دهد.
-                            <br>
-                            خدمات اصلی آموزش در آلاء کاملا رایگان بوده و درآمد خدمات جانبی آن صرف برپا نگه داشتن و دوام این مجموعه عام المنفعه می شود. محصولات ما پیش تر با نام های آلاء و تخته خاک در اختیار مخاطبان قرار می گرفت که برای سهولت در مدیریت و دسترسی کاربران اکنون انحصارا با نام آلاء منتشر می شود.
-                        </p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-9 my-auto">
-                        <p class="text-justify">
-                            دبیرستان دانشگاه صنعتی شریف در سال 1383 تاسیس و زیر نظر دانشگاه صنعتی شریف فعالیت خود را آغاز کرد. فعالیت های آموزشی آلاء با نظارت دبیرستان دانشگاه شریف انجام می شود.
-                        </p>
-                    </div>
-                    <div class="col-md-3">
-                        <img src="{{ asset('/acm/extra/sharif-school-logo.png') }}" class="img-fluid m--img-centered" alt="دبیرستان دانشگاه شریف آلاء"/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
 </div>
 
 <div class="row">
@@ -99,27 +69,3 @@
         </div>
     </div>
 </div>
-
-
-
-{{--<div class="row">--}}
-    
-    {{--    <div class="col-md-4">
-
-            <div class="portlet light certificate-box" >
-                <div class="card-icon">
-                    <a target="_blank" onclick="window.open('https://ecunion.ir/verify/itecsgroup.com?token=35220325acf0232a85b3', 'Popup','toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30')" style="cursor:pointer" ><img src="/acm/extra/kasbo_kar_majazi.png" alt="iranianasnaf.ir"></a>
-                </div>
-                <div class="card-desc">
-                    <span> عضویت اتحادیه کسب و کارهای مجازی </span>
-                </div>
-                <div class="card-title">
-                    --}}{{--<a target="_blank" href="http://iranianasnaf.ir/Forms/Public/CorporateIdentityInquiry/default.aspx"  class="btn yellow bg-font-dark">پروانه کسب</a>--}}{{--
-                    <a target="_blank" onclick="window.open('https://ecunion.ir/verify/itecsgroup.com?token=35220325acf0232a85b3', 'Popup','toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30')" style="cursor:pointer"  class="btn blue bg-font-dark">پروانه کسب</a>
-                </div>
-
-            </div>
-
-        </div>--}}
-
-{{--</div>--}}
