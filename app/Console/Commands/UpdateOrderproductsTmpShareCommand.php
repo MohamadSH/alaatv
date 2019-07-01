3c32682acb398909514562afc24c6098947036df
mohamamad <mohamad.shahrokhi@gmail.com>
2019-07-01 10:27:22 +0430

    CodeCleaning

--- app/Console/Commands/UpdateOrderproductsTmpShareCommand.php
+++ app/Console/Commands/UpdateOrderproductsTmpShareCommand.php
@@ -6,14 +6,14 @@ use App\Orderproduct;
 use App\Repositories\OrderproductRepo;
 use Illuminate\Console\Command;
 
-class UpdateOrderproductsTmpPriceCommand extends Command
+class UpdateOrderproductsTmpShareCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'alaaTv:update-orderproducts-tmpprice';
+    protected $signature = 'alaaTv:update-orderproducts-tmpshare';
 
     /**
      * The console command description.
@@ -44,13 +44,14 @@ class UpdateOrderproductsTmpPriceCommand extends Command
             $orderproducts = Orderproduct::all(); //ToDo : improve performance , pagination could help
         }else{
             // Process new orderproducts with no cache
-            $orderproducts = Orderproduct::whereNull('tmp_final_cost')->get();
+            $orderproducts = Orderproduct::whereNull('tmp_share_order')->get();
         };
 
         if ($this->confirm('Found '.$orderproducts->count().' orderproducts , Do you want to proceed?', true)) {
             $bar = $this->output->createProgressBar($orderproducts->count());
             foreach ($orderproducts as $orderproduct) {
-                $orderproduct->setTmpFinalCost();
+                $finalPrice = $orderproduct->tmp_final_cost;
+                $orderproduct->setShareCost($finalPrice);
                 $bar->advance();
             }
             $bar->finish();
