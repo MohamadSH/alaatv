885597f12058d82d2a6fe5d08566cab6a0ea4774
mohamamad <mohamad.shahrokhi@gmail.com>
2019-07-02 19:04:39 +0430

    live

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -1189,7 +1189,7 @@ class HomeController extends Controller
         $user = $request->user();
         $now = Carbon::now('Asia/Tehran');
         $start = Carbon::parse('2019-07-02 20:00:00','Asia/Tehran');
-        $finish = Carbon::parse('2019-07-02 22:00:00','Asia/Tehran');
+        $finish = Carbon::parse('2019-07-02 22:01:30','Asia/Tehran');
 
         if($user->hasRole('admin'))
         {
