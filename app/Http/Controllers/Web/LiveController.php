365477a00a764412edb405a882882440fad0e71d
mohamamad <mohamad.shahrokhi@gmail.com>
2019-07-20 16:58:59 +0430

    Live

--- app/Http/Controllers/Web/LiveController.php
+++ app/Http/Controllers/Web/LiveController.php
@@ -20,45 +20,56 @@ class LiveController extends Controller
     public function __invoke(Request $request)
     {
         $user = $request->user();
-
+        $message = '';
         $today = Carbon::today()->setTimezone('Asia/Tehran');
         $todayStringDate = $today->toDateString();
         /** @var DayofWeek $dayOfWeek */
         $dayOfWeek = $this->getDayOfWeek($today->dayName)->first();
-        $live = $this->isThereLive($dayOfWeek, $todayStringDate)->first();
+        $liveEvent = $this->isThereLive($dayOfWeek, $todayStringDate)->first();
+        $startTime = $liveEvent->start_time;
+        $finishTime = $liveEvent->finish_time;
+        $title = $liveEvent->title;
 
-        if(!isset($live))
+        if(!isset($liveEvent))
         {
+            $message = 'امروز پخش زنده ای وجود ندارد';
             $live = 'notFound';
-            $message = 'پخش زنده ای یافت نشد';
             return view('pages.liveView' , compact('live', 'message'));
         }
 
-        $start  = Carbon::parse($todayStringDate .' '.$live->start_time,'Asia/Tehran');
-        $finish = Carbon::parse($todayStringDate .' '.$live->finish_time,'Asia/Tehran');
+        $start  = Carbon::parse($todayStringDate .' '.$startTime,'Asia/Tehran');
+        $finish = Carbon::parse($todayStringDate .' '.$finishTime,'Asia/Tehran');
         $now = Carbon::now('Asia/Tehran');
-        if(!$user->hasRole('admin'))
+        if($user->hasRole('admin'))
         {
             if($now->isBefore($start) || $now->between($start, $finish)) {
+                $view = 'pages.liveView';
                 $live = 'on';
             }elseif($now->isAfter($finish)) {
                 $live = 'finished';
+                $message = 'پخش آنلاین به اتمام رسیده است';
+                $view = 'errors.404';
             }
         }else{
             if($now->isBefore($start)) {
                 $live = 'off';
-                $message = 'پخش آنلاین 08:40 امروز شروع میشه';
+                $message = 'پخش آنلاین ساعت '.$startTime.' شروع می شود';
+                $view = 'errors.404';
             }elseif($now->between($start, $finish)){
                 $live = 'on';
-                $message = '';
+                $view = 'pages.liveView';
             }else{
-                $message = 'پخش آنلاید به اتمام رسیده است.';
                 $live = 'off';
+                $message = 'پخش آنلاین به اتمام رسیده است';
+                $view = 'errors.404';
             }
         }
 
-        $poster = $live->poster;
-        return view('pages.liveView' , compact('live', 'message', 'poster'));
+        $xMpegURL = 'https://alaatv.arvanlive.com/hls/test/test.m3u8';
+        $dashXml = 'https://alaatv.arvanlive.com/dash/test/test.mpd';
+        $poster = $liveEvent->poster;
+        $fullVideo = [];
+        return view($view , compact('live', 'message', 'poster' , 'xMpegURL' , 'dashXml' , 'fullVideo' , 'title'));
     }
 
 
