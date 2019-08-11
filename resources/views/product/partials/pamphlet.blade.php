421b08fb606782be7c1faa41766e4004e98040d4
kerasus <aliesmaeeli20@gmail.com>
2019-08-11 19:58:10 +0430

    product show page lazy load pamphlet

--- resources/views/product/partials/pamphlet.blade.php
+++ resources/views/product/partials/pamphlet.blade.php
@@ -14,7 +14,7 @@
                     <div class="row" id="lightgallery">
                         @foreach ($product->samplePhotos as $samplePhoto)
                             <div class="col-3 col-sm-2 col-md-1 m--padding-left-5 m--padding-right-5 m--margin-top-5 a--imageWithCaption" data-src="{{ $samplePhoto->url }}">
-                                <img src="{{ $samplePhoto->url  }}?w=142&h=197" alt="{{ $samplePhoto->title }}" class="img-thumbnail">
+                                <img  src="https://cdn.alaatv.com/loder.jpg?w=1&h=1" data-src="{{ $samplePhoto->url  }}" alt="{{ $samplePhoto->title }}" class="img-thumbnail lazy-image" width="142" height="197">
                                 <div class="a--imageCaptionWarper">
                                     <div class="a--imageCaptionContent">
                                         <div class="a--imageCaptionTitle">نمونه جزوه</div>
