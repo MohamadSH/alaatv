3f128a4ef8e5a95a198360f7f946999f1b0b636e
mohamamad <mohamad.shahrokhi@gmail.com>
2019-08-26 17:54:41 +0430

    content block in product show

--- resources/views/content/show.blade.php
+++ resources/views/content/show.blade.php
@@ -776,47 +776,5 @@
         })(window, document, '35b39d4b-517b-44bc-85c4-44f93242836f');
     </script>
     <script src="{{ mix("/js/content-show.js") }}" type="text/javascript"></script>
-    <script>
-        $('.contentBlock').OwlCarouselType2({
-            OwlCarousel: {
-                center: false,
-                loop: false,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    400: {
-                        items: 2
-                    },
-                    600: {
-                        items: 3
-                    },
-                    800: {
-                        items: 4
-                    },
-                    1000: {
-                        items: 5
-                    }
-                },
-                btnSwfitchEvent: function() {
-                    imageObserver.observe();
-                    gtmEecProductObserver.observe();
-                },
-                onTranslatedEvent: function(event) {
-                    imageObserver.observe();
-                    gtmEecProductObserver.observe();
-                }
-            },
-            grid: {
-                columnClass: 'col-12 col-sm-6 col-md-3 gridItem',
-                btnSwfitchEvent: function() {
-                    imageObserver.observe();
-                    gtmEecProductObserver.observe();
-                }
-            },
-            defaultView: 'OwlCarousel', // OwlCarousel or grid
-            childCountHideOwlCarousel: 4
-        });
-    </script>
 @endsection
 
