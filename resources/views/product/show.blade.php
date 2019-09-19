c43231e5cf4a0957e8f31ed461971af46cb1d537
mohamamad <mohamad.shahrokhi@gmail.com>
2019-09-19 17:52:31 +0430

    UI

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -109,7 +109,7 @@
                                                         <div class="m-portlet__head-caption">
                                                             <div class="m-portlet__head-title">
                                                                 <h3 class="m-portlet__head-text">
-                                                                    ویژگی ها
+                                                                    ویژگی های محصول {{$product->name}}
                                                                 </h3>
                                                             </div>
                                                         </div>
