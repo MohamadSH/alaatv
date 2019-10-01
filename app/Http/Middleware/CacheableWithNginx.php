d1551aaf36be6b095ef54c56e224997ac1fc31a2
sohrabafard <sohrab.a.fard@gmail.com>
2019-10-01 03:45:49 +0330

    add Except Array For Critical Endpoint

--- app/Http/Middleware/CacheableWithNginx.php
+++ app/Http/Middleware/CacheableWithNginx.php
@@ -7,7 +7,11 @@ use Closure;
 class CacheableWithNginx
 {
     private $cookieName = 'nginx_not_cacheable';
-    
+    private $except     = [
+        '/login',
+        '/checkout/review',
+        '/logout',
+    ];
     /**
      * Handle an incoming request.
      *
@@ -18,11 +22,33 @@ class CacheableWithNginx
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user()) {
+        if ($request->user() || $this->inExceptArray($request)) {
             \Cookie::queue(cookie()->forever($this->cookieName, '1'));
         } else {
             \Cookie::queue(cookie()->forget($this->cookieName));
         }
         return $next($request);
+    }
+    
+    /**
+     * Determine if the request has a URI that should pass through CSRF verification.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return bool
+     */
+    protected function inExceptArray($request)
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+            
+            if ($request->fullUrlIs($except) || $request->is($except)) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
