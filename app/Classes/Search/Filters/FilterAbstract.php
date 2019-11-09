36b7d73befe0ac5b94ec3655d7b28c12717626a4
mohamamad <mohamad.shahrokhi@gmail.com>
2019-11-09 16:19:51 +0330

    Code cleaning

--- app/Classes/Search/Filters/FilterAbstract.php
+++ app/Classes/Search/Filters/FilterAbstract.php
@@ -8,44 +8,45 @@
 
 namespace App\Classes\Search\Filters;
 
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Database\Eloquent\Builder;
 use LogicException;
 
 class FilterAbstract implements Filter
 {
     protected $attribute;
-    
+
     protected $relation;
-    
+
     public function __construct()
     {
         if (!isset($this->attribute)) {
             throw new LogicException(get_class($this).' must have a $attribute');
         }
     }
-    
+
     public function apply(Builder $builder, $value, FilterCallback $callback): Builder
     {
         $value = $this->getSearchValue($value);
-        
+
         return $builder->where($this->attribute, 'LIKE', "%".$value."%");
     }
-    
+
     protected function getSearchValue($value)
     {
         return trim($value);
     }
-    
+
     /**
-     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     * @return array|Translator|null|string
      */
     protected function getValueShouldBeSetMessage()
     {
         return trans("filter.value should be set", ["filter" => get_class($this)]);
     }
-    
+
     /**
-     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     * @return array|Translator|null|string
      */
     protected function getValueShouldBeArrayMessage()
     {
