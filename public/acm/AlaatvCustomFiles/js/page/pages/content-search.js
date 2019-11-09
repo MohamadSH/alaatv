34e788c8c4059a08eade35c785b8a453e7c63fb7
kerasus <aliesmaeeli20@gmail.com>
2019-11-09 15:10:07 +0330

    block-set count icon update 2

--- public/acm/AlaatvCustomFiles/js/page/pages/content-search.js
+++ public/acm/AlaatvCustomFiles/js/page/pages/content-search.js
@@ -132,9 +132,9 @@ var Alaasearch = function () {
             '                <span class="a--block-set-count-title">محتوا</span>\n' +
             '                <br>\n' +
             '                <a href="'+widgetLink+'" class="a--block-set-count-icon">\n' +
-            '                    <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" <g="" style="width: 25px;fill: white;">\n' +
-            '        <path d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z" class="style-scope yt-icon" style="color: hsla(0, 0%, 100%, 0.8);/*! fill: red; */"></path>\n' +
-            '      </svg>\n' +
+            '                    <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" <g="" >\n' +
+            '                        <path d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z" class="style-scope yt-icon"></path>\n' +
+            '                    </svg>\n' +
             '                </a>\n' +
             '            </div>\n' +
             '            \n' +
