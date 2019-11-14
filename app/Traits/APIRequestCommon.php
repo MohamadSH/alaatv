d19b7aae526ed286d68bc937caa32837741409d5
mohamamad <mohamad.shahrokhi@gmail.com>
2019-11-14 12:01:01 +0330

    Satra
    fix #3061

--- app/Traits/APIRequestCommon.php
+++ app/Traits/APIRequestCommon.php
@@ -1,31 +1,29 @@
 <?php namespace App\Traits;
 
 use GuzzleHttp\Client;
-use Illuminate\Http\Request;
 use PHPUnit\Framework\Exception;
 use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Exception\GuzzleException;
 
 trait APIRequestCommon
 {
-    public function sendRequest($path, $method, $parameters = [])
+    public function sendRequest($path, $method, $parameters = [] , $headers = [])
     {
         $client  = new Client();
-        $request = new Request();
-        foreach ($parameters as $key => $parameter) {
-            $request->offsetSet($key, $parameter);
-        }
         try {
-            $res = $client->request($method, $path, ['form_params' => $request->all()]);
+            if(empty($headers)){
+                $res = $client->request($method, $path, ['form_params' => $parameters]);
+            }else{
+               $res = $client->request($method, $path, ['query' => $parameters , 'headers' => $headers]);
+            }
         } catch (GuzzleException $e) {
             Log::error($e->getMessage());
             throw new Exception($e->getMessage());
         }
-        
+
         return [
             "statusCode" => $res->getStatusCode(),
-            "result"     => $res->getBody()
-                ->getContents(),
+            "result"     => $res->getBody()->getContents(),
         ];
     }
 }
