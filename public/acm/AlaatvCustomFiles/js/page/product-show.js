11ff7bdab4ebaee6c0e06ca6f0e5e213438da48a
kerasus <aliesmaeeli20@gmail.com>
2019-11-27 10:47:29 +0330

    remove two column with css auto

--- public/acm/AlaatvCustomFiles/js/page/product-show.js
+++ public/acm/AlaatvCustomFiles/js/page/product-show.js
@@ -548,75 +548,80 @@ jQuery(document).ready(function() {
         childCountHideOwlCarousel: 4
     });
 
-    var player = videojs('videoPlayer', {language: 'fa'});
+    if ($('#videoPlayer').length > 0) {
+        var player = videojs('videoPlayer', {language: 'fa'});
 
-    if ($('input[type="hidden"][name="introVideo"]').length > 0) {
-        player.src([
-            {type: "video/mp4", src: $('input[type="hidden"][name="introVideo"]').val()}
-        ]);
+        if ($('input[type="hidden"][name="introVideo"]').length > 0) {
+            player.src([
+                {type: "video/mp4", src: $('input[type="hidden"][name="introVideo"]').val()}
+            ]);
+        }
+
+        player.nuevo({
+            // logotitle:"آموزش مجازی آلاء",
+            // logo:"https://sanatisharif.ir/image/11/135/67/logo-150x22_20180430222256.png",
+            logocontrolbar: '/acm/extra/Alaa-logo.gif',
+            // logoposition:"RT", // logo position (LT - top left, RT - top right)
+            logourl: '//sanatisharif.ir',
+            // related: related_videos,
+            // shareUrl:"https://www.nuevolab.com/videojs/",
+            // shareTitle: "Nuevo plugin for VideoJs Player",
+            // slideImage:"//cdn.nuevolab.com/media/sprite.jpg",
+
+            // videoInfo: true,
+            // infoSize: 18,
+            // infoIcon: "https://sanatisharif.ir/image/11/150/150/favicon_32_20180819090313.png",
+
+            closeallow: false,
+            mute: true,
+            rateMenu: true,
+            resume: true, // (false) enable/disable resume option to start video playback from last time position it was left
+            // theaterButton: true,
+            // timetooltip: true,
+            // mousedisplay: true,
+            endAction: 'related', // (undefined) If defined (share/related) either sharing panel or related panel will display when video ends.
+            container: "inline",
+
+
+            // limit: 20,
+            // limiturl: "http://localdev.alaatv.com/videojs/examples/basic.html",
+            // limitimage : "//cdn.nuevolab.com/media/limit.png", // limitimage or limitmessage
+            // limitmessage: "اگه می خوای بقیه اش رو ببینی باید پول بدی :)",
+
+
+            // overlay: "//domain.com/overlay.html" //(undefined) - overlay URL to display html on each pause event example: https://www.nuevolab.com/videojs/tryit/overlay
+
+        });
+
+        player.hotkeys({
+            volumeStep: 0.1,
+            seekStep: 5,
+            alwaysCaptureHotkeys: true
+        });
+
+        player.pic2pic();
+
+        // player.on('mode',function(event,mode) {
+        //     let width = '100%';
+        //     if(mode=='large') {
+        //         // $('.productDetailesColumns .column1').addClass('order-2');
+        //         // $('.productDetailesColumns .column2').addClass('order-3');
+        //         $('.productDetailesColumns .column3').addClass('order-first');
+        //         $('.productDetailesColumns .column3').removeClass('col-lg-4');
+        //         $('.productDetailesColumns .column3').addClass('col-lg-12');
+        //         $('.productDetailesColumns .column3 .videoPlayerPortlet').css({'width':'60%'});
+        //     } else {
+        //         // $('.productDetailesColumns .column1').removeClass('order-2');
+        //         // $('.productDetailesColumns .column2').removeClass('order-3');
+        //         $('.productDetailesColumns .column3').removeClass('order-first');
+        //         $('.productDetailesColumns .column3').removeClass('col-lg-12');
+        //         $('.productDetailesColumns .column3').addClass('col-lg-4');
+        //         $('.productDetailesColumns .column3 .videoPlayerPortlet').css({'width':'100%'});
+        //     }
+        // });
     }
 
-    player.nuevo({
-        // logotitle:"آموزش مجازی آلاء",
-        // logo:"https://sanatisharif.ir/image/11/135/67/logo-150x22_20180430222256.png",
-        logocontrolbar: '/acm/extra/Alaa-logo.gif',
-        // logoposition:"RT", // logo position (LT - top left, RT - top right)
-        logourl: '//sanatisharif.ir',
-        // related: related_videos,
-        // shareUrl:"https://www.nuevolab.com/videojs/",
-        // shareTitle: "Nuevo plugin for VideoJs Player",
-        // slideImage:"//cdn.nuevolab.com/media/sprite.jpg",
-
-        // videoInfo: true,
-        // infoSize: 18,
-        // infoIcon: "https://sanatisharif.ir/image/11/150/150/favicon_32_20180819090313.png",
-
-        closeallow: false,
-        mute: true,
-        rateMenu: true,
-        resume: true, // (false) enable/disable resume option to start video playback from last time position it was left
-        // theaterButton: true,
-        // timetooltip: true,
-        // mousedisplay: true,
-        endAction: 'related', // (undefined) If defined (share/related) either sharing panel or related panel will display when video ends.
-        container: "inline",
-
-
-        // limit: 20,
-        // limiturl: "http://localdev.alaatv.com/videojs/examples/basic.html",
-        // limitimage : "//cdn.nuevolab.com/media/limit.png", // limitimage or limitmessage
-        // limitmessage: "اگه می خوای بقیه اش رو ببینی باید پول بدی :)",
-
-
-        // overlay: "//domain.com/overlay.html" //(undefined) - overlay URL to display html on each pause event example: https://www.nuevolab.com/videojs/tryit/overlay
-
-    });
-
-    player.hotkeys({
-        volumeStep: 0.1,
-        seekStep: 5,
-        alwaysCaptureHotkeys: true
-    });
-
-    player.pic2pic();
-
-    // player.on('mode',function(event,mode) {
-    //     let width = '100%';
-    //     if(mode=='large') {
-    //         // $('.productDetailesColumns .column1').addClass('order-2');
-    //         // $('.productDetailesColumns .column2').addClass('order-3');
-    //         $('.productDetailesColumns .column3').addClass('order-first');
-    //         $('.productDetailesColumns .column3').removeClass('col-lg-4');
-    //         $('.productDetailesColumns .column3').addClass('col-lg-12');
-    //         $('.productDetailesColumns .column3 .videoPlayerPortlet').css({'width':'60%'});
-    //     } else {
-    //         // $('.productDetailesColumns .column1').removeClass('order-2');
-    //         // $('.productDetailesColumns .column2').removeClass('order-3');
-    //         $('.productDetailesColumns .column3').removeClass('order-first');
-    //         $('.productDetailesColumns .column3').removeClass('col-lg-12');
-    //         $('.productDetailesColumns .column3').addClass('col-lg-4');
-    //         $('.productDetailesColumns .column3 .videoPlayerPortlet').css({'width':'100%'});
-    //     }
-    // });
-
+    if ($('#productInformation .summernote-row .summernote-col').length > 0) {
+        $('#productInformation').css('column-count', '1');
+    }
 });
