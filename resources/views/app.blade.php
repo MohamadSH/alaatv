04947d87fa9421a81110b1144593c14a83df18c6
kerasus <aliesmaeeli20@gmail.com>
2019-12-20 11:11:50 +0330

    firebase update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -58,6 +58,10 @@
         </div>
         <!-- end::Scroll Top -->
 
+
+
+
+
         <!--begin::Global Theme Bundle -->
         <script src="{{ mix('/js/all.js') }}" type="text/javascript"></script>
         <!--end::Global Theme Bundle -->
@@ -75,7 +79,30 @@
                 });
             });
         </script>
+
         @yield('page-js')
+
+
+
+
+
+{{--    <!-- Insert these scripts at the bottom of the HTML, but before you use any Firebase services -->--}}
+
+{{--    <!-- Firebase App (the core Firebase SDK) is always required and must be listed first -->--}}
+{{--    <script src="https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js"></script>--}}
+
+{{--    <!-- If you enabled Analytics in your project, add the Firebase SDK for Analytics -->--}}
+{{--    <script src="https://www.gstatic.com/firebasejs/7.6.0/firebase-analytics.js"></script>--}}
+
+{{--    <!-- Add Firebase products that you want to use -->--}}
+{{--    <script src="https://www.gstatic.com/firebasejs/7.6.0/firebase-auth.js"></script>--}}
+{{--    <script src="https://www.gstatic.com/firebasejs/7.6.0/firebase-firestore.js"></script>--}}
+{{--    <script src="https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js"></script>--}}
+{{--    <script src="{{ asset('/firebase-messaging-sw.js') }}" type="text/javascript"></script>--}}
+
+
+
+
     </body>
     <!-- end::Body -->
 </html>
