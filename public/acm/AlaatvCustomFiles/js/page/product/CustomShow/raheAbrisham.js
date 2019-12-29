0b16ca33766d3d945fdf7ca024794544e2cb77d4
kerasus <aliesmaeeli20@gmail.com>
2019-12-29 13:17:08 +0330

    rahe abrisham -> update

--- public/acm/AlaatvCustomFiles/js/page/product/CustomShow/raheAbrisham.js
+++ public/acm/AlaatvCustomFiles/js/page/product/CustomShow/raheAbrisham.js
@@ -919,7 +919,7 @@ var EntekhabeFarsang = function () {
     function createPamphletItem(data) {
         return '' +
             '<div class="item w-55443211" data-section-id="'+data.section.id+'" data-section-name="'+data.section.name+'">\n' +
-            '  <a href="' + data.link + '">' +
+            '  <a href="' + data.link + '" class="m-link">' +
             '    <div class="pamphletItem">\n' +
             '        <div class="pamphletItem-thumbnail">\n' +
             '            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">\n' +
