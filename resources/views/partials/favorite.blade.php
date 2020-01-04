cd230f9f5df1df8d886d9e87bcbe845ba7780ff4
kerasus <aliesmaeeli20@gmail.com>
2020-01-04 12:14:22 +0330

    fixed #3448

--- resources/views/partials/favorite.blade.php
+++ resources/views/partials/favorite.blade.php
@@ -1,9 +1,9 @@
 @if(isset($favActionUrl) && isset($unfavActionUrl) && isset($isFavored))
     <input type="hidden" name="favoriteActionUrl" value="{{ $favActionUrl }}">
     <input type="hidden" name="unFavoriteActionUrl" value="{{ $unfavActionUrl }}">
-    
+
     <div class="btnFavorite">
-        <img class="btnFavorite-on {{ ($isFavored) ? '' : 'a--d-none' }}" src="https://cdn.alaatv.com/upload/fav-on.svg" width="50">
-        <img class="btnFavorite-off {{ ($isFavored) ? 'a--d-none' : '' }}" src="https://cdn.alaatv.com/upload/fav-off.svg" width="50">
+        <img class="btnFavorite-on {{ ($isFavored) ? '' : 'a--d-none' }}" src="https://cdn.alaatv.com/upload/bookmark-on.svg" width="50">
+        <img class="btnFavorite-off {{ ($isFavored) ? 'a--d-none' : '' }}" src="https://cdn.alaatv.com/upload/bookmark-off.svg" width="50">
     </div>
 @endif
