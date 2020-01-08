ba4f8025d8a6d0bb639635d1ff79e8783b6fe83e
kerasus <aliesmaeeli20@gmail.com>
2020-01-08 12:09:15 +0330

    favorite off icon update

--- resources/views/partials/favorite.blade.php
+++ resources/views/partials/favorite.blade.php
@@ -3,7 +3,7 @@
     <input type="hidden" name="unFavoriteActionUrl" value="{{ $unfavActionUrl }}">
 
     <div class="btnFavorite">
-        <img class="btnFavorite-on {{ ($isFavored) ? '' : 'a--d-none' }}" src="https://cdn.alaatv.com/upload/bookmark-on.svg" width="50">
-        <img class="btnFavorite-off {{ ($isFavored) ? 'a--d-none' : '' }}" src="https://cdn.alaatv.com/upload/bookmark-off.svg" width="50">
+        <img class="btnFavorite-on  lazy-image {{ ($isFavored) ? '' : 'a--d-none' }}" data-src="https://cdn.alaatv.com/upload/bookmark-on.svg" width="50" height="50">
+        <img class="btnFavorite-off lazy-image {{ ($isFavored) ? 'a--d-none' : '' }}" data-src="https://cdn.alaatv.com/upload/bookmark-off.svg" width="50" height="50">
     </div>
 @endif
