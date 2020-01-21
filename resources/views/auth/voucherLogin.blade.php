167ad98c85585ea54920597bb7588e77897432d0
kerasus <aliesmaeeli20@gmail.com>
2020-01-21 13:03:53 +0330

    update 1

---
+++ resources/views/auth/voucherLogin.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fa" direction="rtl" style="direction: rtl">
+    <!-- begin::Head -->
+    <head>
+        @include('partials.html-head')
+        @yield('page-head')
+    </head>
+    <!-- end::Head -->
+
+    <!-- begin::Body -->
+    <body>
+    </body>
+</html>
