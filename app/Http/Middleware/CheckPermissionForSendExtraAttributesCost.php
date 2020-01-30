d7a72d338b898f7e8705d10802f9a4d8f4fcdb23
mohamamad <mohamad.shahrokhi@gmail.com>
2020-01-30 14:03:39 +0330

    Orderproduct code cleaning

--- app/Http/Middleware/CheckPermissionForSendExtraAttributesCost.php
+++ app/Http/Middleware/CheckPermissionForSendExtraAttributesCost.php
@@ -2,20 +2,16 @@
 
 namespace App\Http\Middleware;
 
-use App\Http\Controllers\OrderController;
 use App\Product;
+use App\Traits\OrderproductTrait;
 use Closure;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class CheckPermissionForSendExtraAttributesCost
 {
-    /**
-     * @var OrderController
-     */
-    private $orderController;
+    use OrderproductTrait;
 
     private $user;
 
@@ -23,12 +19,10 @@ class CheckPermissionForSendExtraAttributesCost
      * OrderCheck constructor.
      *
      * @param Request         $request
-     * @param OrderController $controller
      */
-    public function __construct(Request $request, OrderController $controller)
+    public function __construct(Request $request)
     {
-        $this->orderController = $controller;
-        $this->user            = $request->user();
+        $this->user = $request->user();
     }
 
     /**
@@ -57,38 +51,5 @@ class CheckPermissionForSendExtraAttributesCost
         $request->offsetSet('parentProduct', $product);
 
         return $next($request);
-    }
-
-    /**
-     * @param Request $request
-     * @param Product $product
-     *
-     * @return Collection|null
-     */
-    private function getAttributesValuesFromProduct(Request $request, Product $product): ?Collection
-    {
-        $extraAttributes   = $request->get('extraAttribute');
-        $extraAttributesId = array_column($extraAttributes, 'id');
-        $attributesValues  = $product->getAttributesValueByIds($extraAttributesId);
-
-        return $attributesValues;
-    }
-
-    /**
-     * @param Request    $request
-     * @param Collection $attributesValues
-     */
-    public function syncExtraAttributesCost(Request $request, Collection $attributesValues)
-    {
-        $extraAttributes = $request->get('extraAttribute');
-        foreach ($attributesValues as $key => $attributesValue) {
-            foreach ($extraAttributes as $key1 => $extraAttribute) {
-                if ($extraAttribute['id'] == $attributesValue['id']) {
-                    $extraAttributes[$key1]['cost']   = $attributesValue->pivot->extraCost;
-                    $extraAttributes[$key1]['object'] = $attributesValue;
-                }
-            }
-        }
-        $request->offsetSet("extraAttribute", $extraAttributes);
     }
 }
